9ac922180dbfbf4d65e88f32b09d5e316cd7c210
NathanGinisty <nginisty.pro@gmail.com>
2021-10-04 23:56:34 +0200

    Changed how I load Item

--- js/product.js
+++ js/product.js
@@ -1,48 +1,49 @@
-const apiUrl = "http://localhost:3000/api/cameras/"
+import * as main from './main.js';
 
-var itemId = window.location.href.split('#')[1];
+var itemID = window.location.href.split('#')[1];
 
-function GetItemFromID(index)
+function LoadItemFromID(_id)
 {
-    fetch(apiUrl + index)
-    .then(data => data.json())
-    .then(data =>{
+    main.GetData(LoadItem, main.apiData.url + main.apiData.table + "/" + _id);
+}
+
+function LoadItem(_data)
+{
+    // rename to the new _id        
+    document.getElementById("prdt-0").setAttribute("id", "prdt-" + itemID);
+    document.getElementById("prdt-name-0").setAttribute("id", "prdt-name-" + itemID);
+    document.getElementById("prdt-price-0").setAttribute("id", "prdt-price-" + itemID);
+    document.getElementById("prdt-description-0").setAttribute("id", "prdt-description-" + itemID);
+    document.getElementById("prdt-img-0").setAttribute("id", "prdt-img-" + itemID);
+    document.getElementById("prdt-lenses-0-0").setAttribute("id", "prdt-lenses-" + itemID + "-0");
+    
+    // set new value onto the new product
+    document.getElementById("prdt-name-" + itemID).innerHTML = _data.name;
+    document.getElementById("prdt-img-" + itemID).src = _data.imageUrl;
+    document.getElementById("prdt-description-" + itemID).innerHTML = _data.description;
+    
+    let newPrice = String(_data.price);
+    document.getElementById("prdt-price-" + itemID).innerHTML = newPrice.slice(0,-2)+ "." + newPrice.slice(-2,newPrice.length)+ "€";
+    
+    // get and set all the lenses value (ver1)
+    var j = 0;
+    var srcLenses = document.getElementById("prdt-lenses-" + itemID + "-0");
+    var lastLenses = srcLenses;
+    
+    _data.lenses.forEach(e => {
+        if (j > 0)
+        {
+            var newLenses = srcLenses.cloneNode(true);
+            lastLenses.parentNode.insertBefore(newLenses, lastLenses);
+            document.getElementById("prdt-lenses-" + itemID + "-0").setAttribute("id", "prdt-lenses-" + itemID + "-" + j);
+        }
+        document.getElementById("prdt-lenses-" + itemID + "-" + j).innerHTML = e;
+        document.getElementById("prdt-lenses-" + itemID + "-" + j).addEventListener("click", function(){SelectLenses(this)});
         
-        // rename to the new index        
-        document.getElementById("prdt-0").setAttribute("id", "prdt-" + index);
-        document.getElementById("prdt-name-0").setAttribute("id", "prdt-name-" + index);
-        document.getElementById("prdt-price-0").setAttribute("id", "prdt-price-" + index);
-        document.getElementById("prdt-description-0").setAttribute("id", "prdt-description-" + index);
-        document.getElementById("prdt-img-0").setAttribute("id", "prdt-img-" + index);
-        document.getElementById("prdt-lenses-0-0").setAttribute("id", "prdt-lenses-" + index + "-0");
-        
-        // set new value onto the new product
-        document.getElementById("prdt-name-" + index).innerHTML = data.name;
-        document.getElementById("prdt-img-" + index).src = data.imageUrl;
-        document.getElementById("prdt-description-" + index).innerHTML = data.description;
-        
-        let newPrice = String(data.price);
-        document.getElementById("prdt-price-" + index).innerHTML = newPrice.slice(0,-2)+ "." + newPrice.slice(-2,newPrice.length)+ "€";
-        
-        // get and set all the lenses value (ver1)
-        var j = 0;
-        var srcLenses = document.getElementById("prdt-lenses-" + index + "-0");
-        var lastLenses = srcLenses;
-        
-        data.lenses.forEach(e => {
-            if (j > 0)
-            {
-                var newLenses = srcLenses.cloneNode(true);
-                lastLenses.parentNode.insertBefore(newLenses, lastLenses);
-                document.getElementById("prdt-lenses-" + index + "-0").setAttribute("id", "prdt-lenses-" + index + "-" + j);
-            }
-            document.getElementById("prdt-lenses-" + index + "-" + j).innerHTML = e;
-            
-            lastLenses = document.getElementById("prdt-lenses-" + index + "-" + j);
-            j++;
-        })
-        
+        lastLenses = document.getElementById("prdt-lenses-" + itemID + "-" + j);
+        j++;
     })
+    
 }
 
 function SelectLenses(_element)
@@ -61,19 +62,21 @@ function AddToCart()
     // Create another object
     var alreadyExist = false;
     var itemLenses = window.location.href.split('#')[2];
-
+    
     //parseInt()
-    var a = document.getElementById("prdt-price-" + itemId).innerHTML.split(".")[0];
-    var b = document.getElementById("prdt-price-" + itemId).innerHTML.split(".")[1].split('€')[0];
+    var a = document.getElementById("prdt-price-" + itemID).innerHTML.split(".")[0];
+    var b = document.getElementById("prdt-price-" + itemID).innerHTML.split(".")[1].split('€')[0];
     var trueValuePrice = parseFloat(a) + (parseFloat(b)/100);
-
-    var productToAdd = {
-        id: itemId,
-        name: document.getElementById("prdt-name-" + itemId).innerHTML,
+    
+    var productToAdd =
+    {
+        id: itemID,
+        name: document.getElementById("prdt-name-" + itemID).innerHTML,
         price: trueValuePrice,
         lenses: itemLenses,
-        lensesText: document.getElementById("prdt-lenses-" + itemId + "-" + itemLenses).innerHTML,
-        quantity: 1};
+        lensesText: document.getElementById("prdt-lenses-" + itemID + "-" + itemLenses).innerHTML,
+        quantity: 1
+    };
     
     // Check if already exist. If yes, increase quantity
     cart.forEach(e => {
@@ -96,4 +99,6 @@ function AddToCart()
 /* ----------------------------------------------------------- */
 
 
-GetItemFromID(itemId);
+LoadItemFromID(itemID);
+document.getElementById("button-addtocart").addEventListener("click", AddToCart);
+
